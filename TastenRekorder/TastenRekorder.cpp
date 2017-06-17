d73275b08171dfce96adcbb28ecd8ceffe49c7af
Ali Denno <aliedenno@gmail.com>
2017-06-17 21:50:55 +0200

    Update TastenRekorder.cpp
    Add message to prevent console from appearing

--- TastenRekorder/TastenRekorder.cpp
+++ TastenRekorder/TastenRekorder.cpp
@@ -2,13 +2,18 @@
 //
 
 #include "stdafx.h"
-
+#include <windows.h>
 #include <iostream>
 
 using namespace std;
 
 int main()
-{
-	cout << "Hello world!" << endl;
+{	
+	// Compiler Flag to be solved here
+	MSG Msg;
+	while (GetMessage(&Msg, NULL, 0, 0)) {
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
 	return 0;
 }
